3ebbd99d3fd32aabbc1a33638f08b4aa5cc8ede2
mtokudome <makoto.tokudome@liferay.com>
2021-01-21 15:37:09 -0800

    source formatter

--- docs/dxp/7.x/en/liferay-internals/extending-liferay/overriding-osgi-services/reconfiguring-components-to-use-your-osgi-service/resources/liferay-m1t1.zip/m1t1-web/src/main/java/com/acme/m1t1/M1T1Portlet.java
+++ docs/dxp/7.x/en/liferay-internals/extending-liferay/overriding-osgi-services/reconfiguring-components-to-use-your-osgi-service/resources/liferay-m1t1.zip/m1t1-web/src/main/java/com/acme/m1t1/M1T1Portlet.java
@@ -17,7 +17,7 @@ import org.osgi.service.component.annotations.Reference;
 		"com.liferay.portlet.display-category=category.sample",
 		"com.liferay.portlet.instanceable=true",
 		"javax.portlet.display-name=M1T1 Portlet",
-		"javax.portlet.security-role-ref=power-user,user",
+		"javax.portlet.security-role-ref=power-user,user"
 	},
 	service = Portlet.class
 )
@@ -33,7 +33,6 @@ public class M1T1Portlet extends GenericPortlet {
 
 		printWriter.println(_m1t1.doSomething());
 	}
-
 
 	@Reference(unbind = "-")
 	private M1T1 _m1t1;
