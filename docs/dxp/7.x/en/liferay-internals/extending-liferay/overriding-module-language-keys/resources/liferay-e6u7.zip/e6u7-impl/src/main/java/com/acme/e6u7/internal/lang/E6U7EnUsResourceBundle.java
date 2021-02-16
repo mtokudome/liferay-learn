2c49392a35102f15146e7a49da9a5a863ce1ed8c
mtokudome <makoto.tokudome@liferay.com>
2021-02-16 15:40:42 -0800

    LRDOCS-7659 removed getLabel method, removed other language keys

--- docs/dxp/7.x/en/liferay-internals/extending-liferay/overriding-module-language-keys/resources/liferay-e6u7.zip/e6u7-impl/src/main/java/com/acme/e6u7/internal/lang/E6U7EnUsResourceBundle.java
+++ docs/dxp/7.x/en/liferay-internals/extending-liferay/overriding-module-language-keys/resources/liferay-e6u7.zip/e6u7-impl/src/main/java/com/acme/e6u7/internal/lang/E6U7EnUsResourceBundle.java
@@ -1,16 +1,13 @@
 package com.acme.e6u7.internal.lang;
 
-import com.liferay.portal.kernel.language.LanguageUtil;
 import com.liferay.portal.kernel.language.UTF8Control;
-import com.liferay.portal.kernel.util.ResourceBundleUtil;
 
 import java.util.Enumeration;
-import java.util.Locale;
 import java.util.ResourceBundle;
 
 import org.osgi.service.component.annotations.Component;
 
-@Component(property = "language.id=en", service = ResourceBundle.class)
+@Component(property = "language.id=en_US", service = ResourceBundle.class)
 public class E6U7EnUsResourceBundle extends ResourceBundle {
 
 	@Override
@@ -18,19 +15,12 @@ public class E6U7EnUsResourceBundle extends ResourceBundle {
 		return _resourceBundle.getKeys();
 	}
 
-	/* public String getLabel(Locale locale) {
-		ResourceBundle resourceBundle = ResourceBundleUtil.getBundle(
-			"content.Language_en", locale, getClass());
-
-		return LanguageUtil.get(resourceBundle, "add-blog-entry");
-	} */
-
 	@Override
 	protected Object handleGetObject(String key) {
 		return _resourceBundle.getObject(key);
 	}
 
 	private final ResourceBundle _resourceBundle = ResourceBundle.getBundle(
-		"content.Language_en", UTF8Control.INSTANCE);
+		"content.Language_en_US", UTF8Control.INSTANCE);
 
 }
