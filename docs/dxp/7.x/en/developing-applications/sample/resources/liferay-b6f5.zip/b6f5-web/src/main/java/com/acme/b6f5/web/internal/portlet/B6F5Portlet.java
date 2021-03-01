ba6b65ed8e47f0bb00b58e295375933001c695c0
mtokudome <makoto.tokudome@liferay.com>
2021-03-01 10:26:53 -0800

    changed module folder

---
+++ docs/dxp/7.x/en/developing-applications/sample/resources/liferay-b6f5.zip/b6f5-web/src/main/java/com/acme/b6f5/web/internal/portlet/B6F5Portlet.java
@@ -0,0 +1,25 @@
+package com.acme.b6f5.web.internal.porlet;
+
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.display-category=category.sample",
+		"com.liferay.portlet.instanceable=true",
+		"javax.portlet.display-name=BLADE Language Web Portlet",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.init-param.view-template=/view.jsp",
+		"javax.portlet.name=com_acme_b6f5_web_internal_portlet",
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=power-user,user"
+	},
+	service = Portlet.class
+)
+public class B6F5Portlet extends MVCPortlet {
+}
