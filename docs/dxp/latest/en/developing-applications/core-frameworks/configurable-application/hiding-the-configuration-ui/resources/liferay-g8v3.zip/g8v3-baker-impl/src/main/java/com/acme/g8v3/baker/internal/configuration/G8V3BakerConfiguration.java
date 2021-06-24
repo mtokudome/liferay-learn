a0aa2daebd06788d50fe8460221151bde206dd11
mtokudome <makoto.tokudome@liferay.com>
2021-06-24 07:42:54 -0700

    LRDOCS-9529 able baker

---
+++ docs/dxp/latest/en/developing-applications/core-frameworks/configurable-application/hiding-the-configuration-ui/resources/liferay-g8v3.zip/g8v3-baker-impl/src/main/java/com/acme/g8v3/baker/internal/configuration/G8V3BakerConfiguration.java
@@ -0,0 +1,14 @@
+package com.acme.g8v3.baker.internal.configuration;
+
+import aQute.bnd.annotation.metatype.Meta;
+
+@Meta.OCD(
+	id = "com.acme.g8v3.baker.internal.configuration.G8V3BakerConfiguration",
+	localization = "content/Language", name = "g8v3-baker-configuration-name"
+)
+public interface G8V3BakerConfiguration {
+
+	@Meta.AD(name = "advanced-settings", required = false)
+	public String advancedSettings();
+
+}
--- docs/dxp/latest/en/developing-applications/core-frameworks/configurable-application/hiding-the-configuration-ui/resources/liferay-g8v3.zip/g8v3-baker-impl/src/main/java/com/acme/g8v3/baker/internal/configuration/admin/display/G8V3BakerConfigurationVisibilityController.java
+++ docs/dxp/latest/en/developing-applications/core-frameworks/configurable-application/hiding-the-configuration-ui/resources/liferay-g8v3.zip/g8v3-baker-impl/src/main/java/com/acme/g8v3/baker/internal/configuration/admin/display/G8V3BakerConfigurationVisibilityController.java
@@ -1,4 +1,6 @@
-package com.acme.g8v3.internal.configuration.admin.display;
+package com.acme.g8v3.baker.internal.configuration.admin.display;
+
+import com.acme.g8v3.able.internal.configuration.G8V3AbleConfiguration;
 
 import com.liferay.configuration.admin.display.ConfigurationVisibilityController;
 import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
@@ -6,21 +8,25 @@ import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClass
 import java.io.Serializable;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 @Component(
-	property = "configuration.pid=com.acme.g8v3.internal.configuration.G8V3Configuration",
+	property = "configuration.pid=com.acme.g8v3.baker.internal.configuration.G8V3BakerConfiguration",
 	service = ConfigurationVisibilityController.class
 )
 
-public class G8V3ConfigurationVisibilityController
+public class G8V3BakerConfigurationVisibilityController
 	implements ConfigurationVisibilityController {
 
 		@Override
 		public boolean isVisible(
 			ExtendedObjectClassDefinition.Scope scope, Serializable scopePK) {
 
-			return scope.equals(ExtendedObjectClassDefinition.Scope.COMPANY);
+			return g8v3AbleConfiguration.enableAdvancedSettings();
 
 		}
+
+		@Reference
+		private G8V3AbleConfiguration g8v3AbleConfiguration;
 
 }
