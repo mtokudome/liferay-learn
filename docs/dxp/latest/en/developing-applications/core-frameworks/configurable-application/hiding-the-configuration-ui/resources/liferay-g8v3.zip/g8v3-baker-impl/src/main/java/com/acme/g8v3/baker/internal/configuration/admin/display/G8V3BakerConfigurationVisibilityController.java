a4d175e551e1a00220b4f0d53e8eb3ff469a3a79
mtokudome <makoto.tokudome@liferay.com>
2021-06-24 10:37:49 -0700

    LRDOCS-9529 activate method

--- docs/dxp/latest/en/developing-applications/core-frameworks/configurable-application/hiding-the-configuration-ui/resources/liferay-g8v3.zip/g8v3-baker-impl/src/main/java/com/acme/g8v3/baker/internal/configuration/admin/display/G8V3BakerConfigurationVisibilityController.java
+++ docs/dxp/latest/en/developing-applications/core-frameworks/configurable-application/hiding-the-configuration-ui/resources/liferay-g8v3.zip/g8v3-baker-impl/src/main/java/com/acme/g8v3/baker/internal/configuration/admin/display/G8V3BakerConfigurationVisibilityController.java
@@ -4,10 +4,15 @@ import com.acme.g8v3.able.internal.configuration.G8V3AbleConfiguration;
 
 import com.liferay.configuration.admin.display.ConfigurationVisibilityController;
 import com.liferay.portal.configuration.metatype.annotations.ExtendedObjectClassDefinition;
+import com.liferay.portal.configuration.metatype.bnd.util.ConfigurableUtil;
 
 import java.io.Serializable;
 
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Modified;
 import org.osgi.service.component.annotations.Reference;
 
 @Component(
@@ -17,6 +22,13 @@ import org.osgi.service.component.annotations.Reference;
 
 public class G8V3BakerConfigurationVisibilityController
 	implements ConfigurationVisibilityController {
+
+		@Activate
+		@Modified
+		protected void activate(Map<Object, Object> properties) {
+			g8v3AbleConfiguration = ConfigurableUtil.createConfigurable(
+				G8V3AbleConfiguration.class, properties);
+		}
 
 		@Override
 		public boolean isVisible(
